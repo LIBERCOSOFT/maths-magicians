2c328f55e2d2299bf184d710c8fbdf59678115a0
LIBERCOSOFT <akinspreciousola@gmail.com>
2022-10-25 14:41:43 +0100

    Implement calculation logic functionality

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,45 +1,63 @@
 import React from 'react';
+import calculate from '../logic/calculate';
 
 export class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      total: 0,
+      next: null,
+      operation: null,
+    };
+  }
+
+  handleClick = (e) => {
+    this.setState((state) => calculate(state, e.target.innerText));
   }
 
   render() {
+    const { total, next, operation } = this.state;
     return (
       <>
-        <div className="display"> 0 </div>
+        <div className="display">
+          {' '}
+          {total}
+          {' '}
+          {operation}
+          {' '}
+          {next}
+          {' '}
+        </div>
 
         <div className="buttonContainer">
           <div>
-            <button type="button">AC</button>
-            <button type="button">+/-</button>
-            <button type="button">%</button>
-            <button type="button">+</button>
+            <button type="button" onClick={this.handleClick}>AC</button>
+            <button type="button" onClick={this.handleClick}>+/-</button>
+            <button type="button" onClick={this.handleClick}>%</button>
+            <button type="button" onClick={this.handleClick}>+</button>
           </div>
           <div>
-            <button type="button">7</button>
-            <button type="button">8</button>
-            <button type="button">9</button>
-            <button type="button">x</button>
+            <button type="button" onClick={this.handleClick}>7</button>
+            <button type="button" onClick={this.handleClick}>8</button>
+            <button type="button" onClick={this.handleClick}>9</button>
+            <button type="button" onClick={this.handleClick}>x</button>
           </div>
           <div>
-            <button type="button">4</button>
-            <button type="button">5</button>
-            <button type="button">6</button>
-            <button type="button">-</button>
+            <button type="button" onClick={this.handleClick}>4</button>
+            <button type="button" onClick={this.handleClick}>5</button>
+            <button type="button" onClick={this.handleClick}>6</button>
+            <button type="button" onClick={this.handleClick}>-</button>
           </div>
           <div>
-            <button type="button">1</button>
-            <button type="button">2</button>
-            <button type="button">3</button>
-            <button type="button">+</button>
+            <button type="button" onClick={this.handleClick}>1</button>
+            <button type="button" onClick={this.handleClick}>2</button>
+            <button type="button" onClick={this.handleClick}>3</button>
+            <button type="button" onClick={this.handleClick}>+</button>
           </div>
           <div>
-            <button type="button" className="zero">0</button>
-            <button type="button">.</button>
-            <button type="button">=</button>
+            <button type="button" className="zero" onClick={this.handleClick}>0</button>
+            <button type="button" onClick={this.handleClick}>.</button>
+            <button type="button" onClick={this.handleClick}>=</button>
           </div>
         </div>
 
