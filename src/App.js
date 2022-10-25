829e9150fae17984071fdef22b00606f578020bb
LIBERCOSOFT <akinspreciousola@gmail.com>
2022-10-25 05:06:11 +0100

    Fix linter error

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import Calculator from './components/Calculator';
+import React from 'react';
+
+import { Calculator } from './components/Calculator';
 import './App.css';
 
-function App() {
-  return (
-    <Calculator />
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <Calculator />
+      </div>
+    );
+  }
 }
 
 export default App;
