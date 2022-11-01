c944f5beacdee247ea8947ceb44bf1ae04b49d6a
LIBERCOSOFT <akinspreciousola@gmail.com>
2022-11-01 16:33:29 +0100

    Add home and quote page with react router and styling

---
+++ src/pages/Quote.js
@@ -0,0 +1,11 @@
+/* eslint-disable max-len */
+import React from 'react';
+
+const Quote = () => (
+  <div className="quoteContainer">
+    <h2>Mathematics is not about numbers, quations, computations, or algorithms; It is about understanding. </h2>
+    <h4>- William Paul Thurston</h4>
+  </div>
+);
+
+export default Quote;
